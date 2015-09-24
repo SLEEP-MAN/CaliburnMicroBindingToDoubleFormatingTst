04d4a6054243e5f39ed6c7999baf9097b5d9aa47
Maciej Nowicki <maciej.nowicki@pl.abb.com>
2015-09-24 07:27:45 +0200

    Check issue #122

--- CaliburnMicroBindingToDoubleFormatingTst/ShellViewModel.cs
+++ CaliburnMicroBindingToDoubleFormatingTst/ShellViewModel.cs
@@ -2,6 +2,7 @@ namespace CaliburnMicroBindToDoubleFormattingTest {
     public class ShellViewModel : Caliburn.Micro.PropertyChangedBase, IShell
     {
         public double BoundByName { get; set; }
+        public double BoundByPathWorksAsBoundByName { get; set; }
         public double BoundByPath { get; set; }
         public double BoundByPathWithFormatting { get; set; }
     }
